6b7c81291356eb98fb6a6a0d59773de438d030f2
arun pratap singh <arunpratap836@gmail.com>
2022-02-02 12:13:43 +0530

    link-list,stack,queue

---
+++ phase1/session6/StackQueqe/QueueExample.java
@@ -0,0 +1,24 @@
+package com.simplilearn.StackQueqe;
+
+import java.util.Queue;
+
+import com.simplilearn.linklist.LinkedList;
+
+public class QueueExample {
+	public static void main(String[] args) 
+	{
+	        		Queue<String> locationsQueue = (Queue<String>) new LinkedList();
+	locationsQueue.add("Kolkata");
+	        		locationsQueue.add("Patna");
+	        		locationsQueue.add("Delhi");
+	        		locationsQueue.add("Gurgaon");
+	        		locationsQueue.add("Noida");
+	System.out.println("Queue is : " + locationsQueue);
+	        		System.out.println("Head of Queue : " + locationsQueue.peek());
+	        		locationsQueue.remove();
+	        		System.out.println("After removing Head of Queue : " + locationsQueue);
+	        		System.out.println("Size of Queue : " + locationsQueue.size());
+	    	}
+
+
+}
