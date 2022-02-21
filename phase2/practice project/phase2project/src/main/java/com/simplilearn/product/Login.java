7f82db6dcc7afdbca0274a7d7b08005089ace70a
arun pratap singh <arunpratap836@gmail.com>
2022-02-21 11:39:29 +0530

    assistant,project

---
+++ phase2/practice project/phase2project/src/main/java/com/simplilearn/product/Login.java
@@ -0,0 +1,75 @@
+package com.simplilearn.product;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String url="jdbc:mysql://localhost:3306/Arun";
+		String uname="root";
+		String pass="Rudra@1234";
+		
+		response.setContentType("text/html");
+		
+		String pId = request.getParameter("p_ID");
+	
+		PrintWriter out = response.getWriter();
+		
+		String query="select * from product_Details where ProductId=?";
+		out.print("<h1>Displaying the Product Details...</h1>");
+		out.print("<table border='1'><tr><th>Product Id</th><th>Product Name</th><th>Product Price</th></tr>");
+		
+		try {
+	      Class.forName("com.mysql.cj.jdbc.Driver");
+	      Connection dbCon = DriverManager.getConnection(url, uname, pass);
+	      PreparedStatement st=  dbCon.prepareStatement(query);
+	      
+	      st.setString(1, pId);
+	      
+	      ResultSet rs =st.executeQuery();
+	      
+	      while(rs.next()) {
+	    	  
+	    	  out.print("<tr><td>");
+	    	  out.println(rs.getInt(1));
+	    	  out.print("</td>");
+	    	  out.print("<td>");
+	    	  out.print(rs.getString(2));
+	    	  out.print("</td>");
+	    	  out.print("<td>");
+	    	  out.print(rs.getInt(3));
+	    	  out.print("</td>");
+	    	  out.print("</tr>");
+	    
+	    	  
+			}
+	      
+		}
+		catch(Exception e){
+			
+			System.out.println("Some Issue : "+ e.getMessage());
+			
+			
+		}
+		
+		out.print("</table>");
+		
+	}
+}
